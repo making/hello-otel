ec26fae6f23038f7edc34c3123ae079233ae67fe
Toshiaki Maki <tmaki@vmware.com>
2021-03-30 12:47:07 +0900

    Remove RequestDumperFilter

--- order-api/src/main/java/com/example/order/OrderApiApplication.java
+++ order-api/src/main/java/com/example/order/OrderApiApplication.java
@@ -1,11 +1,8 @@
 package com.example.order;
 
-import org.apache.catalina.filters.RequestDumperFilter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -13,10 +10,5 @@ public class OrderApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApiApplication.class, args);
-	}
-
-	@Bean
-	public RequestDumperFilter requestDumperFilter() {
-		return new RequestDumperFilter();
 	}
 }
--- payment-api/src/main/java/com/example/payment/PaymentApiApplication.java
+++ payment-api/src/main/java/com/example/payment/PaymentApiApplication.java
@@ -1,10 +1,7 @@
 package com.example.payment;
 
-import org.apache.catalina.filters.RequestDumperFilter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentApiApplication {
@@ -12,10 +9,4 @@ public class PaymentApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApiApplication.class, args);
 	}
-
-	@Bean
-	public RequestDumperFilter requestDumperFilter() {
-		return new RequestDumperFilter();
-	}
-
 }
